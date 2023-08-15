d1de1c0a040ff1e9711a67e7c0ec8c97f75df092
andreas-grimm <andreas.grimm@gricom.eu>
2023-08-15 09:31:30 +0200

    Initial Commit

---
+++ GD-Basic-C/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GD-Basic-C
+//
+//  Created by Andreas Grimm on 15/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
